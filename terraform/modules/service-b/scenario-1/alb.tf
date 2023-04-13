c43e570f19b239dd2cee397183649cdd23733ae2
yashodhah <y.dominatz@gmail.com>
2023-04-13 19:43:16 +0530

    folder structure change

--- terraform/modules/service-b/scenario-1/alb.tf
+++ terraform/modules/service-b/scenario-1/alb.tf
@@ -2,14 +2,12 @@ module "alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "6.2.0"
 
-  name                  = "example-alb"
+  name                  = "service-b-alb"
   vpc_id                = module.vpc.vpc_id
   subnets               = module.vpc.public_subnets
   security_groups       = [module.vpc.default_security_group_id]
   enable_http2          = true
-  access_logs_enabled   = true
-  access_logs_s3_bucket = "my-s3-bucket"
-  access_logs_s3_prefix = "logs/"
+  access_logs_enabled   = false
   tags = {
     Environment = "dev"
   }
